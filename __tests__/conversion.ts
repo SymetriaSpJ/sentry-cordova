306114e9bacb13e6ebb4fcdc4b90dab8f87a5a3f
Robert Klimczak <rbtklima@gmail.com>
2019-04-17 12:13:47 +0200

    v3

--- __tests__/conversion.ts
+++ __tests__/conversion.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 describe('JS conversion check', () => {
   test('Do not require @sentry/browser', () => {
-    const content = fs.readFileSync('dist/js/sentry-cordova.bundle.js');
+    const content = fs.readFileSync('js/sentry-cordova.bundle.js');
     // expect(content.toString().match(/^[^\*]*@sentry\/browser/gm)).toBeFalsy();
     expect(content.toString().match(/ require\(/)).toBeFalsy();
   });
